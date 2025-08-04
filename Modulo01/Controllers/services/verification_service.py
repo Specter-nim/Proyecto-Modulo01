2e561c1514f72b8e718439427c56465d379a3bdc
Juanarmando Victor Jesus Linares Lau <juanarmandolau@gmail.com>
2025-08-04 11:45:20 -0500

    correccion 2.4

--- Modulo01/Controllers/services/verification_service.py
+++ Modulo01/Controllers/services/verification_service.py
@@ -1,19 +1,14 @@
 from rest_framework.response import Response
 from rest_framework import status
-from Auth.models import UserVerificationToken  # Ajusta si está en otro módulo
-
+from Controllers.models import UserVerificationToken  # ACA PAVO
 def verify_code(user, code):
     try:
         token_obj = UserVerificationToken.objects.get(user=user, code=code)
-
         if token_obj.is_expired():
             return Response({'message': 'El código ha expirado.'}, status=status.HTTP_400_BAD_REQUEST)
-
         user.is_verified = True
         user.save()
         token_obj.delete()
-
         return Response({'message': 'Código verificado correctamente.'}, status=status.HTTP_200_OK)
-
     except UserVerificationToken.DoesNotExist:
         return Response({'message': 'Código inválido.'}, status=status.HTTP_404_NOT_FOUND)
