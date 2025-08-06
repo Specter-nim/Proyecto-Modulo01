f8621fd50f71a4aaf7121a78c5dafe8abf86648b
Juanarmando Victor Jesus Linares Lau <juanarmandolau@gmail.com>
2025-08-06 12:22:46 -0500

    eliminacion del auth

--- Auth/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import permissions
-
-class AllowAll(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return True  # Lo puedes personalizar como Laravel's `authorize()`
