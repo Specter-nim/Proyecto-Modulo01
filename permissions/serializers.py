5eca30c27447fa6a39ac04497e55c66cb231069c
CH4IS7IANFLOO <chistiancarreal215@gmail.com>
2025-08-17 12:08:27 +0700

    Delete permissions directory

--- permissions/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Permission
-
-class PermissionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Permission
-        fields = ['id', 'name', 'content_type', 'codename', 'detail'] 
